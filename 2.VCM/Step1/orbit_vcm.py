29ec9765966d06ca20906ae9aabb7144a91ffa8b
Vincent Noel <vincent.noel@gmail.com>
2014-04-25 12:28:15 +0200

    working framework for creating vcm files.

--- 2.VCM/Step1/orbit_vcm.py
+++ 2.VCM/Step1/orbit_vcm.py
@@ -4,36 +4,44 @@
 # Created by VNoel Thu Apr 24 17:07:38 2014
 
 import numpy as np
-import calipso_local
+import calipso_l2
+import dimarray as da
 
-vcm_alt = np.r_[0:22:0.3]
+vcm_alt = np.r_[0:22.3:0.3]
 
 
-def vcm_from_layers():
+def vcm_from_layers(nl, base, top, havg, ltype, only_havg=None):
     # FIXME
     pass
+
+    # for clouds, feature type == layer_type == 2
+
 
 
 def vcm_dataset_from_l2_orbit(filename):
     
-    l2 = Cal2(filename)
+    print 'Creating vcm dataset from l2 file ' + filename
+    
+    l2 = calipso_l2.Cal2(filename)
     lon, lat = l2.coords()
     tai_time = l2.time()
     nl, base, top = l2.layers()
     havg = l2.horizontal_averaging()
-    # for clouds, feature type == layer_type == 2
     ltype = l2.layer_type()
     
     havgs_vcm = [5, 20, 80]
     vertical_cloud_masks = da.Dataset()
     
-    for havg_vcm in havgs_vcm:
-        vcm = vcm_from_layers(nl, base, top, havg, only_havg=havg_vcm)
-        vcm_name = 'vcm_' + '%02d' % (havg_vcm)
-        vertical_cloud_masks[vcn_name] = da.DimArray(vcm, axes=[('tai_time', tai_time), ('altitude', vcm_alt))
+    time_axis = ('tai_time', tai_time)
+    alt_axis = ('altitude', vcm_alt)
     
-    vertical_cloud_masks['lon'] = da.DimArray(lon, axes=('tai_time', tai_time))
-    vertical_cloud_masks['lat'] = da.DimArray(lat, axes=('tai_time', tai_time))
+    for havg_vcm in havgs_vcm:
+        vcm = vcm_from_layers(nl, base, top, havg, ltype, only_havg=havg_vcm)
+        vcm_name = 'vcm_' + '%02d' % (havg_vcm)
+        vertical_cloud_masks[vcm_name] = da.DimArray(vcm, (time_axis, alt_axis))
+    
+    vertical_cloud_masks['lon'] = da.DimArray(lon, (time_axis,))
+    vertical_cloud_masks['lat'] = da.DimArray(lat, (time_axis,))
 
     outname = 'vcm_' + l2.orbit + '.nc4'
     return vertical_cloud_masks, outname
@@ -41,12 +49,37 @@ def vcm_dataset_from_l2_orbit(filename):
     
 def vcm_file_from_l2_orbit(filename, where='./'):
     
+    import os
+    
     vcm, outname = vcm_dataset_from_l2_orbit(filename)
     # check if where exists, fix it if not
-    vcm.write_nc(where + outname)
+    if not os.path.isdir(where):
+        print 'Creating dir ' + where
+        os.mkdir(where)
+    vcm.write_nc(where + outname, mode='w')
     
     
-def main(l2file='bla'):
+# test functions
+# to run tests : py.test orbit_vcm.py
+    
+def test_vcm_creation_for_a_day():
+    
+    import calipso_local
+    import glob
+    
+    out = './test.out/'
+    
+    l2files = calipso_local.l2_night_files(2009,1,1)
+    assert l2files != []
+    
+    for l2file in l2files:
+        vcm_file_from_l2_orbit(l2file, where=out)
+    
+    outfiles = glob.glob(out + '/*.nc4')
+    assert len(outfiles)==len(l2files)
+    
+    
+def main(l2file):
     
     vcm_file_from_l2_orbit(l2file, where='./')
 
