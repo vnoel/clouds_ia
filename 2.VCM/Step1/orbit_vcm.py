f0f0c4856dfc045fa2752bb78220bbc5e34058fb
Vincent Noel <vincent.noel@gmail.com>
2014-04-28 15:18:15 +0200

    add cloud mask creation code, fix naming of output files

--- 2.VCM/Step1/orbit_vcm.py
+++ 2.VCM/Step1/orbit_vcm.py
@@ -7,16 +7,42 @@ import numpy as np
 import calipso_l2
 import dimarray as da
 
+# altitude vector
 vcm_alt = np.r_[0:22.3:0.3]
+nalt = vcm_alt.shape[0]
+
+# averaging levels to include
+havgs_vcm = [5, 20, 80]
 
 
 def vcm_from_layers(nl, base, top, havg, ltype, only_havg=None):
-    # FIXME
-    pass
-
+    
+    nprof = base.shape[0]
+    vcm = np.zeros([nprof, nalt], dtype='uint8')
+    
+    # clean up unwanted layers first
+    
     # for clouds, feature type == layer_type == 2
+    base[ltype != 2] = -9999.
+    # keep only requested havg
+    if only_havg is not None:
+        base[havg != only_havg] = -9999.
+    
+    for i in xrange(nprof):
+        
+        if nl[i] == 0:
+            continue
+        
+        for j in xrange(nl[i]):
+            
+            if base[i,j] < 0:
+                continue
 
-
+            idx = (vcm_alt >= base[i,j]) & (vcm_alt < top[i,j])
+            vcm[i, idx] = 1
+    
+    return vcm
+    
 
 def vcm_dataset_from_l2_orbit(filename):
     
@@ -29,7 +55,6 @@ def vcm_dataset_from_l2_orbit(filename):
     havg = l2.horizontal_averaging()
     ltype = l2.layer_type()
     
-    havgs_vcm = [5, 20, 80]
     vertical_cloud_masks = da.Dataset()
     
     time_axis = ('tai_time', tai_time)
@@ -37,13 +62,13 @@ def vcm_dataset_from_l2_orbit(filename):
     
     for havg_vcm in havgs_vcm:
         vcm = vcm_from_layers(nl, base, top, havg, ltype, only_havg=havg_vcm)
-        vcm_name = 'vcm_' + '%02d' % (havg_vcm)
+        vcm_name = 'vcm_%02dkm' % (havg_vcm)
         vertical_cloud_masks[vcm_name] = da.DimArray(vcm, (time_axis, alt_axis))
     
     vertical_cloud_masks['lon'] = da.DimArray(lon, (time_axis,))
     vertical_cloud_masks['lat'] = da.DimArray(lat, (time_axis,))
 
-    outname = 'vcm_' + l2.orbit + '.nc4'
+    outname = 'vcm_' + l2.id + '.nc4'
     return vertical_cloud_masks, outname
     
     
@@ -62,6 +87,23 @@ def vcm_file_from_l2_orbit(filename, where='./'):
 # test functions
 # to run tests : py.test orbit_vcm.py
     
+def test_vcm_nprof():
+    
+    import calipso_local
+    import glob
+    
+    out = './test.out'
+    
+    l2files = calipso_local.l2_night_files(2009,1,1)
+    vcm, outname = vcm_dataset_from_l2_orbit(l2files[0])
+    # assert vcm['altitude'].shape[0] == vcm_alt.shape[0]
+    nprof = vcm['lon'].shape[0]
+    assert nprof > 0
+    assert vcm['vcm_05km'].shape[0] == nprof
+    print vcm['vcm_05km'].ix[0,:]
+    assert np.all(np.isfinite(vcm['vcm_05km'][:,:]))
+    
+    
 def test_vcm_creation_for_a_day():
     
     import calipso_local
@@ -75,7 +117,8 @@ def test_vcm_creation_for_a_day():
     for l2file in l2files:
         vcm_file_from_l2_orbit(l2file, where=out)
     
-    outfiles = glob.glob(out + '/*.nc4')
+    outfiles = glob.glob(out + '*.nc4')
+
     assert len(outfiles)==len(l2files)
     
     
--- 2.VCM/Step1/scan_l2_files.py
+++ 2.VCM/Step1/scan_l2_files.py
@@ -4,27 +4,42 @@
 # Created by VNoel on 2014-04-24
 
 from orbit_vcm import vcm_file_from_l2_orbit
+from datetime import datetime, timedelta
+import calipso_local
+
 
 def main(year=2007, month=None, day=None):
     
     if day is not None and month is not None:
+        year, month, day = int(year), int(month), int(day)
         start = datetime(year, month, day)
         end = start
     elif day is None and month is not None:
+        year, month = int(year), int(month)
         start = datetime(year, month, 1)
         end = start + timedelta(days=31)
     else:
+        year = int(year)
         start = datetime(year, 1, 1)
         end = datetime(year, 12, 31)
 
     current = start
     while current <= end:
         
-        l2files = calipso_local.l2files(current.year, current.month, current.day)
+        l2files = calipso_local.l2_night_files(current.year, current.month, current.day)
         for l2file in l2files:
-            vcm_file_from_l2_orbit(l2file, where='./out/%04d%02/' % (current.year, current.month))        
+            vcm_file_from_l2_orbit(l2file, where='./out/%04d%02d/' % (current.year, current.month))        
         
         current += timedelta(days=1)
+
+
+def test_day_run():
+    
+    import glob
+    
+    main(2009, 1, 1)
+    outfiles = glob.glob('out/200901/*.nc4')
+    assert len(outfiles) > 0
 
 
 if __name__ == '__main__':
