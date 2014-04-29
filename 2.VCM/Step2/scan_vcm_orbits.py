92277f51b5b6a35149efa2dfffcd5378623d7c96
Vincent Noel <vincent.noel@gmail.com>
2014-04-29 15:15:49 +0200

    refactor a bit, fix a bug

--- 2.VCM/Step2/scan_vcm_orbits.py
+++ 2.VCM/Step2/scan_vcm_orbits.py
@@ -8,7 +8,22 @@ from grid_vcm import grid_vcm_file_from_vcm_orbit
 import glob
     
 
-def main(year=2007, month=None, day=None):
+def process_vcm_orbits_period(start, end):
+
+    current = start
+    while current <= end:
+        
+        inpath = './in/%04d%02d/' % (current.year, current.month)
+        mask = 'vcm_%04d-%02d-%02d*.nc4' % (current.year, current.month, current.day)
+        vcm_files = glob.glob(inpath + mask)
+        print inpath, mask, current, len(vcm_files)
+        for vcm_file in vcm_files:
+            grid_vcm_file_from_vcm_orbit(vcm_file, where='out/%04d%02d/' % (current.year, current.month))
+        
+        current += timedelta(days=1)
+
+
+def main(year=2009, month=None, day=None):
 
     if day is not None and month is not None:
         year, month, day = int(year), int(month), int(day)
@@ -23,19 +38,15 @@ def main(year=2007, month=None, day=None):
         start = datetime(year, 1, 1)
         end = datetime(year, 12, 31)
 
-    current = start
-    while current <= end:
-        
-        inpath = './in/%04d%02d/' % (current.year, current.month)
-        mask = 'vcm_%04d%02d%02d*.nc4' % (current.year, current.month, current.day)
-        vcm_files = glob.glob(inpath + mask)
-        print current, len(vcm_files)
-        for vcm_file in vcm_files:
-            grid_vcm_file_from_vcm_orbit(vcm_file)
-        
-        current += timedelta(days=1)
+    process_vcm_orbits_period(start, end)
 
 
 def test_day_run():
     
-        main(2009,1,1)
+    orbit_files = glob.glob('in/200901/vcm_2009-01-01*.nc4')
+    main(2009,1,1)
+    grid_files = glob.glob('out/200901/vcm_lat_*.nc4')
+        
+    assert len(orbit_files)==len(grid_files)
+    assert False
+        
