f609e8a2c11d99b64b67ac3bf0b31ccf9c7aaa8f
Vincent Noel <vincent.noel@gmail.com>
2014-05-10 14:45:55 +0200

    smaller vertical bins

--- 2.VCM/Step1/orbit_vcm_cal.py
+++ 2.VCM/Step1/orbit_vcm_cal.py
@@ -8,7 +8,7 @@ import calipso_l2
 import dimarray as da
 
 # altitude vector
-vcm_alt = np.r_[0:22.3:0.3]
+vcm_alt = np.r_[0:19.5+0.03:0.03]
 nalt = vcm_alt.shape[0]
 
 # averaging levels to include
