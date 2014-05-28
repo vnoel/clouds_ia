8e240d2d0c08d92e25cc7fc831e8bc2153fb4c66
Vincent Noel <vincent.noel@gmail.com>
2014-05-28 23:13:16 +0200

    plot simpler maps for diagnostics

--- 2.VCM/Step2/show_vcm_maps.py
+++ 2.VCM/Step2/show_vcm_maps.py
@@ -3,20 +3,22 @@
 
 # Created by VNoel on 2014-04-28
 
+import numpy as np
 import dimarray as da
 import matplotlib.pyplot as plt
 
 
-def pcolor_vcm(vcm, title=None):
+def pcolor_vcm(x, y, vcmarray, title=None):
 
     from mpl_toolkits.basemap import Basemap
 
     m = Basemap()
 
     plt.figure(figsize=[10,5])
-    m.pcolormesh(vcm.labels[0], vcm.labels[1], vcm.values.T, cmap='BuPu')
-    m.drawcoastlines()
+    plt.pcolormesh(x, y, vcmarray.T)
+    #m.drawcoastlines()
     plt.colorbar()
+    plt.clim(0, 60000)
     if title is not None:
         plt.title(title)
 
@@ -30,11 +32,12 @@ def aggregate_arrays_from_files(files, array_name, summed_along=None):
     for f in files:
 
         data = da.read_nc(f)
+        if array_name not in data:
+            continue
         array = data[array_name]
 
         if summed_along is not None:
-            array = array.sum(axis=summed_along)
-            
+            array = array.sum(axis=summed_along)            
         if aggregated is None:
             aggregated = 1. * array
         else:
@@ -48,32 +51,37 @@ def aggregate_arrays_from_files(files, array_name, summed_along=None):
 def show_file(filename, title):
     
     data = da.read_nc(filename)
-    vcm05 = data['vcm_csat+cal333-80']
-    vcm_lonlat = vcm05.sum(axis='altitude')
+    vcm05 = data['vcm_cal333']
+    vcm_lonlat = 1. * vcm05.sum(axis='altitude') / data['nprof']
     
     pcolor_vcm(vcm_lonlat, title)
 
 
 def show_files(files, title):
     
-    vcm = aggregate_arrays_from_files(files, 'vcm_csat+cal333-80', 'altitude')
+    vcm = aggregate_arrays_from_files(files, 'vcm_cal333', 'altitude')
     nprof = aggregate_arrays_from_files(files, 'nprof')
-    print vcm, nprof
-    cloudypoints = vcm / nprof
-    cloudypoints[nprof==0] = 0
-    pcolor_vcm(cloudypoints, 'cloudy points in profiles : ' + title)
-    plt.clim(0,120)
+    
+    cloudypoints = np.ma.masked_where(nprof.values==0, 1. * vcm.values / nprof.values)
+    cloudypoints = np.ma.masked_invalid(cloudypoints)
+    
+    cloudypoints = vcm.values
+    
+    pcolor_vcm(vcm.labels[0], vcm.labels[1], cloudypoints, 'cloudy points in profiles : ' + title)
+    #plt.clim(0,100)
 
 
-def main(vcm_grid_file='./test.out/200801/vcm_grid_2008-01-01.nc4'):
+def main(vcm_grid_file='./out/200707/vcm_grid_2007-07-01.nc4'):
     
     import glob
     
-    show_file(vcm_grid_file, vcm_grid_file)
+    #show_file(vcm_grid_file, vcm_grid_file)
     
-    #mask = 'out/200801/vcm_grid_*.nc4'
-    #grid_files = glob.glob(mask)
-    #show_files(grid_files, '200801')
+    mask = 'out/200707/vcm_grid_*.nc4'
+    grid_files = glob.glob(mask)
+    grid_files.sort()
+
+    show_files(grid_files, '200701-200703')
     
     plt.show()
     
