0e96b8859e6b01c1bedd2b71b04d9beced5bacff
Vincent Noel <vincent.noel@gmail.com>
2014-06-02 13:50:33 +0200

    make it pass tests

--- 4.CF_Maps/cf_vcm.py
+++ 4.CF_Maps/cf_vcm.py
@@ -6,8 +6,9 @@
 import numpy as np
 import dimarray as da
 
+vcm_names = 'vcm_csat+cal333-5', 'vcm_csat+cal333-20', 'vcm_csat+cal333-80', 'vcm_cal333'
 
-def grid_vcm_from_vcm_orbit(vcm_orbit, lstep=2.):
+def cf_from_vcm_orbit(vcm_orbit, lstep=2.):
     
     # create vcm_3d grid
     lonbins = np.r_[-180:180+lstep:lstep]
@@ -19,26 +20,19 @@ def grid_vcm_from_vcm_orbit(vcm_orbit, lstep=2.):
     plon = data['lon'].values
     plat = data['lat'].values
     
-    if 'vcm_csat+cal333-5' not in data or 'vcm_csat+cal333-20' not in data or 'vcm_csat+cal333-80' not in data:
+    if not all(name in data for name in vcm_names):
         print 'Warning : some vcms are not present in file. Contained data :'
         print data
         return None
     
-    altitude = data['vcm_csat+cal333-5'].labels[1]
+    altitude = data[vcm_names[0]].labels[1]
     nalt = altitude.shape[0]
     
     out = da.Dataset()
 
-    for field in data:
+    for vcm_name in vcm_names:
         
-        if not field.startswith('vcm_'):
-            continue
-        
-        this_vcm = data[field].values
-        # we're binning profiles at 333m res
-        # in one day, ~750 5km profiles in a 2x2 box.
-        # needs at least uint16.
-        vcm_3d = np.zeros([nlon, nlat, nalt], dtype='uint16')
+        this_vcm = data[vcm_name].values
         nprof = np.zeros([nlon, nlat], dtype='uint16')
         cprof = np.zeros([nlon, nlat], dtype='uint16')
         
@@ -61,42 +55,38 @@ def grid_vcm_from_vcm_orbit(vcm_orbit, lstep=2.):
                 # number of profiles in the box
                 nprof[ilon, ilat] = this_nprof
                 
-                # vertical cloud mask in the box
-                vcm_3d[ilon, ilat, :] = np.sum(vcm_slice[idx,:], axis=0)
-                
                 # number of cloudy profiles in the box
                 cloudyprofileflags = np.sum(vcm_slice[idx,:], axis=1) > 0
                 cprof[ilon, ilat] = np.sum(cloudyprofileflags)
                 
         
-        out[field] = da.DimArray(vcm_3d, labels=[lonbins, latbins, altitude], dims=['lon', 'lat', 'altitude'])
-        out[field+'_cprof'] = da.DimArray(cprof, labels=[lonbins, latbins], dims=['lon', 'lat'])
+        out[vcm_name + '_cprof'] = da.DimArray(cprof, labels=[lonbins, latbins], dims=['lon', 'lat'])
         if 'nprof' not in out:
             out['nprof'] = da.DimArray(nprof, labels=[lonbins, latbins], dims=['lon', 'lat'])
     
     return out
 
 
-def grid_vcm_file_from_vcm_orbit(vcm_orbit, where='./out/'):
+def cf_file_from_vcm_orbit(vcm_orbit, where='./out/'):
     
     import os
     
-    dataset = grid_vcm_from_vcm_orbit(vcm_orbit)
+    dataset = cf_from_vcm_orbit(vcm_orbit)
     
-    outname = 'vcm_grid_' + vcm_orbit[-25:]
+    outname = 'cf_' + vcm_orbit[-25:]
     if not os.path.isdir(where):
         print 'Creating dir ' + where
         os.mkdir(where)
     dataset.write_nc(where + outname, mode='w', zlib=True, complevel=9)
 
 
-def grid_vcm_file_from_vcm_orbits(vcm_orbits, outname, where='./out'):
+def cf_file_from_vcm_orbits(vcm_orbits, outname, where='./out'):
     
     import os
     
     dataset = None
     for vcm_orbit in vcm_orbits:
-        out = grid_vcm_from_vcm_orbit(vcm_orbit)
+        out = cf_from_vcm_orbit(vcm_orbit)
         if out is None:
             return
         if dataset is None:
@@ -112,23 +102,13 @@ def grid_vcm_file_from_vcm_orbits(vcm_orbits, outname, where='./out'):
     dataset.write_nc(where + outname, mode='w', zlib=True, complevel=9)
 
 
-def test_orbit():
-    
-    import os
-    
-    vcm_orbit = './in/200801/vcm_2008-01-01T01-30-23ZN.nc4'
-    assert os.path.isfile(vcm_orbit)
-    grid_vcm_file_from_vcm_orbit(vcm_orbit, where='./test.out/')
-    assert os.path.isfile('./test.out/vcm_grid_2008-01-01T01-30-23ZN.nc4')
-    
-    
 def test_orbits():
     
     import glob, os
 
-    vcm_orbits = glob.glob('./in/200801/vcm_2008-01-01*.nc4')
+    vcm_orbits = glob.glob('./in/200701/vcm_2007-01-01*.nc4')
     assert len(vcm_orbits) > 1
 
-    grid_vcm_file_from_vcm_orbits(vcm_orbits, 'vcm_grid_2008-01-01.nc4', where='./test.out/')
+    cf_file_from_vcm_orbits(vcm_orbits, 'cf_2007-01-01.nc4', where='./test.out/')
     
-    assert os.path.isfile('./test.out/vcm_grid_2008-01-01.nc4')
+    assert os.path.isfile('./test.out/cf_2007-01-01.nc4')
