f005cde977ca78b747ebd3d3107230b4b109c5fe
Vincent Noel <vincent.noel@gmail.com>
2014-06-05 13:19:55 +0200

    fetch results from icare and plot meridional width

---
+++ 3c.Width/fetch.sh
@@ -0,0 +1,1 @@
+scp access:clouds_ia/3c.Width/tropic_width.npz .
---
+++ 3c.Width/show_width.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#encoding:utf-8
+
+# Forked by VNoel on 2014-06-04
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main(infile='tropic_width.npz'):
+    npz = np.load(infile)
+    tmin, tmax, time = npz['tmin'], npz['tmax'], npz['datetimes']
+    
+    plt.figure(figsize=[15,10])
+    plt.subplot(2,1,1)
+    plt.plot(time, tmin, label='South boundary')
+    plt.plot(time, tmax, label='North boundary')
+    plt.ylabel('Latitude')
+    plt.legend(loc='center right')
+    plt.grid()
+    
+    plt.subplot(2,1,2)
+    plt.plot(time, tmax-tmin)
+    plt.ylabel('Tropics meridional height')
+    
+    plt.show()
+    
+
+if __name__ == '__main__':
+    import plac
+    plac.call(main)
