e9ad1df9d6ad1b4db7c995d2dadac0494f9e8d0f
Vincent Noel <vincent.noel@gmail.com>
2014-06-13 16:36:36 +0200

    more reliable tropic width detection

--- 3c.Width/tropic_width.py
+++ 3c.Width/tropic_width.py
@@ -4,6 +4,7 @@
 # Created by V. Noel [LMD/CNRS] on 2014-06-04
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 def tropic_width(lat, alt, vcm, height=16.):
     
@@ -19,16 +20,21 @@ def tropic_width(lat, alt, vcm, height=16.):
 def tropic_width3(lat, alt, vcm):
     
     cover_top = cloud_cover_top(alt, vcm)
-    idx = (lat > -10) & (lat < 10)
-    height = np.mean(cover_top[idx]) - 2.
+    idx = (lat > -30) & (lat < 30)
     
+    # sorted cover tops between + and - 30 degrees
+    # 6000 points = 60 degrees = 6000 km
+    # 100km average = 100 highest points
+    sorted_tops = np.sort(cover_top[idx])
+    trop_ceiling = np.mean(sorted_tops[-100:])
+    height = trop_ceiling - 2.
+    
+    print('Tropic ceiling = {} km'.format(trop_ceiling))
     print('Cutting tropic hat at {} km'.format(height))
+
+    idx = (cover_top > height) & (lat > -40) & (lat < 40)
     
-    ialt = np.argmin(np.abs(alt-height))
-    vcmslice = vcm[:,ialt]
-    idx = (vcmslice > 0.05) & (lat > -60) & (lat < 60)
-    
-    return np.min(lat[idx]), np.max(lat[idx])
+    return np.min(lat[idx]), np.max(lat[idx]), trop_ceiling, height
     
     
 def tropic_width2(lat, alt, vcm):
