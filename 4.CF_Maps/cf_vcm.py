4c9dc96e715888267360c038e6987c327f30ab28
Vincent Noel <vincent.noel@gmail.com>
2014-06-23 17:20:15 +0200

    this is twice faster and conceptually cleaner

--- 4.CF_Maps/cf_vcm.py
+++ 4.CF_Maps/cf_vcm.py
@@ -6,7 +6,7 @@
 import numpy as np
 import dimarray as da
 
-vcm_names = 'csat', 'cal333', 'cal333+cal05', 'cal333+cal05+cal20', 'cal333+cal05+cal20+cal80+csat'
+vcm_names = ['cal333+cal05+cal20+cal80+csat']
 
 
 def combine_vcms(origin, target_name, output_so_far=None):
@@ -17,7 +17,7 @@ def combine_vcms(origin, target_name, output_so_far=None):
         names = target_name.split('+')
         if not all(name in origin for name in names):
             print 'Warning : some vcms are not present in file. Contained data :'
-            print data
+            print origin
             return None        
             
         # check if we are not simply adding a vcm to a sum already present
@@ -28,69 +28,52 @@ def combine_vcms(origin, target_name, output_so_far=None):
                 output = output_so_far[previous_step].values + origin[names[-1]]
                 return output
 
-        output = origin[names[0]]
+        output = origin[names[0]].values
         for name in names[1:]:
-            output += origin[name]
+            output += origin[name].values
         # FIXME : need to check cloudsat data here
-        np.clip(output, -1, 1, out=output.values)
+        np.clip(output, -1, 1, out=output)
 
     return output
 
 
-def cf_from_vcm_orbit(vcm_orbit, lstep=2.):
+def cf_from_vcm_orbit(vcm_orbit, altrange, lstep=2.):
     
     # create vcm_3d grid
     lonbins = np.r_[-180:180+lstep:lstep]
     latbins = np.r_[-90:90+lstep:lstep]
-    nlon, nlat = lonbins.shape[0], latbins.shape[0]
+    nlon, nlat = lonbins.shape[0]-1, latbins.shape[0]-1
     
     # read data
     data = da.read_nc(vcm_orbit)
     plon = data['lon'].values.astype('float32')
     plat = data['lat'].values.astype('float32')
         
-    altitude = data[vcm_names[0]].labels[1]
-    nalt = altitude.shape[0]
+    altitude = data['cal333'].labels[1]
+    altidx = (altitude >= altrange[0]) & (altitude < altrange[1])
     
     out = da.Dataset()
 
+    # number of profiles in grid
+    h, xx, yy = np.histogram2d(plon, plat, bins=(lonbins, latbins))
+    out['nprof'] = da.DimArray(h, labels=[lonbins[:-1], latbins[:-1]], dims=['lon', 'lat'])
+    out['nprof'].longname = 'Number of measured profiles'
+
     for vcm_name in vcm_names:
         
+        # number of cloudy profiles in grid and altitude range
+
         this_vcm = combine_vcms(data, vcm_name)
         assert this_vcm is not None
-        nprof = np.zeros([nlon, nlat], dtype='uint16')
-        cprof = np.zeros([nlon, nlat], dtype='uint16')
         
-        for ilon, lon in enumerate(lonbins):
-            
-            idxlon = (plon >= lon) & (plon < (lon + lstep))
-            if idxlon.sum() == 0:
-                continue
-            
-            vcm_slice = this_vcm[idxlon,:]
-            lat_slice = plat[idxlon]
-            
-            for ilat, lat in enumerate(latbins):
-            
-                idx = (lat_slice >= lat) & (lat_slice < (lat + lstep))
-                this_nprof = np.sum(idx)
-                if this_nprof == 0:
-                    continue
-            
-                # number of profiles in the box
-                nprof[ilon, ilat] = this_nprof
-                
-                # number of cloudy profiles in the box
-                cloudyprofileflags = np.sum(vcm_slice[idx,:], axis=1) > 0
-                cprof[ilon, ilat] = np.sum(cloudyprofileflags)
-                
-        if 'nprof' not in out:
-            out['nprof'] = da.DimArray(nprof, labels=[lonbins, latbins], dims=['lon', 'lat'])
-            out['nprof'].longname = 'Number of measured profiles'
-
+        vcm_slice = this_vcm[:, altidx]
+        cloudy_profile = np.sum(vcm_slice, axis=1)
+        np.clip(cloudy_profile, 0, 1, out=cloudy_profile)
+        
+        h, xx, yy = np.histogram2d(plon, plat, bins=(lonbins, latbins), weights=cloudy_profile)
         outname = vcm_name + '_cprof'
-        out[outname] = da.DimArray(cprof, labels=[lonbins, latbins], dims=['lon', 'lat'])
-        out[outname].longname = 'Number of cloudy profiles from cloud mask = ', vcm_name
+        out[outname] = da.DimArray(h, labels=[lonbins[:-1], latbins[:-1]], dims=['lon', 'lat'])
+        out[outname].longname = 'Number of cloudy profiles from cloud mask = ' + vcm_name
     
     return out
 
@@ -108,13 +91,13 @@ def cf_file_from_vcm_orbit(vcm_orbit, where='./out/'):
     dataset.write_nc(where + outname, mode='w', zlib=True, complevel=9)
 
 
-def cf_file_from_vcm_orbits(vcm_orbits, outname, where='./out'):
+def cf_file_from_vcm_orbits(vcm_orbits, altrange, outname, where='./out'):
     
     import os
     
     dataset = None
     for vcm_orbit in vcm_orbits:
-        out = cf_from_vcm_orbit(vcm_orbit)
+        out = cf_from_vcm_orbit(vcm_orbit, altrange)
         if out is None:
             return
         if dataset is None:
@@ -127,6 +110,9 @@ def cf_file_from_vcm_orbits(vcm_orbits, outname, where='./out'):
     if not os.path.isdir(where):
         print 'Creating dir ' + where
         os.mkdir(where)
+    
+    print('Saving ' + where + outname)
+    dataset.altrange = '{:f} - {:f}'.format(altrange[0], altrange[1])
     dataset.write_nc(where + outname, mode='w', zlib=True, complevel=9)
 
 
