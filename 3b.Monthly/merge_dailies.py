1f5508fba0cc7df668a57d13f6a1aa78a20ac0a0
Vincent Noel <vincent.noel@gmail.com>
2014-06-27 13:07:17 +0200

    use a moving average window

--- 3b.Monthly/merge_dailies.py
+++ 3b.Monthly/merge_dailies.py
@@ -3,62 +3,86 @@
 
 # Forked by VNoel on 2014-06-02
 
+from datetime import datetime, timedelta
 import numpy as np
 import dimarray as da
 
 
 def dayfiles_sum_dataset(files):
     
+    names = ['cal333+cal05+cal20+cal80+csat', 'cal333+cal05+cal20+cal80+csat_cprof']
     aggregated = da.Dataset()
+    for name in names:
+        data = da.read_nc(files, name, axis='filename')
+        data = data.sum(axis='filename')
+        aggregated[name] = data
+    return aggregated
     
-    for f in files:
+    
+def dayfiles_sum_dataset_old(files):
+    
+    aggregated = da.Dataset()
+    names = ['cal333+cal05+cal20+cal80+csat', 'cal333+cal05+cal20+cal80+csat_cprof']
 
-        data = da.read_nc(f)
-        for array_name in data:
-            
-            # array = data[array_name]
-
-            if array_name not in aggregated:
-                aggregated[array_name] = data[array_name]
-            else:
-                aggregated[array_name] += data[array_name]
+    for name in names:
+        data = da.read_nc(files[0], name)
+        for f in files[1:]:
+            data += da.read_nc(f, name)
+        aggregated[name] = data
         
     return aggregated
 
 
 def dayfiles_to_monthfile(files, outfile):
     
-    monthly_sum = dayfiles_sum_dataset(files)
+    monthly_sum = dayfiles_sum_dataset_old(files)
+    print 'Saving ' + outfile
     monthly_sum.write_nc(outfile, 'w')
 
 
-def scan_months(year, months, where='./out/'):
+def scan_period(start, end, window=40, step=7, where='./out.{:02d}/', indir='./in/'):
     
     import glob, os
     
-    for month in months:
-        mask = 'in/%04d%02d/*.nc4' % (year, month)
-        dayfiles = glob.glob(mask)
-        dayfiles.sort()
+    current = start
+    while current < end:
+        
+        # create list of files for current averaging window
+        
+        c = current - timedelta(days=window/2)
+        this_end = current + timedelta(days=window/2)
+        files = []
+        while c < this_end:
+            this_mask = indir + '/{:04d}{:02d}/vcm_zonal_{:04d}-{:02d}-{:02d}.nc4'.format(c.year, c.month, c.year, c.month, c.day)
+            files += glob.glob(this_mask)
+            c += timedelta(days=1)
+            
+        if len(files)==0:
+            print 'no dailies for {:04d}{:02d}, skipping'.format(c.year, c.month)
+            current += timedelta(days=step)
+            continue
+        
+        where = where.format(window)
         if not os.path.isdir(where):
             print('Creating '+where)
             os.mkdir(where)
-        if not os.path.isdir(where+'%04d' % year):
-            print('Creating '+where + '%04d' % year)
-            os.mkdir(where + '%04d' % year)
-        outfile = where + '%04d/vcm_zonal_%04d%02d.nc4' % (year, year, month)
-        dayfiles_to_monthfile(dayfiles, outfile)
+        if not os.path.isdir(where + '%04d' % current.year):
+            print('Creating '+where + '%04d' % current.year)
+            os.mkdir(where + '%04d' % current.year)
+        outfile = where + '%04d/vcm_zonal_%04d%02d%02d.nc4' % (current.year, current.year, current.month, current.day)
+        print current, ' : aggregating %d files' % len(files)
+        dayfiles_to_monthfile(files, outfile)
+        
+        current += timedelta(days=step)
     
 
 def main(year=2007, month=None):
     
-    if month is None:
-        months = np.r_[1:13]
-    else:
-        months = [int(month)]
     year = int(year)
+    start = datetime(year, 1, 1)
+    end = datetime(year + 1, 1, 1)
         
-    scan_months(year, months)
+    scan_period(start, end)
     
 
 if __name__ == '__main__':
