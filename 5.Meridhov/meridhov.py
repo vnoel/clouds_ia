a6a6317cf8d72cd6f234bbde9e48fab079c21229
Vincent Noel <vincent.noel@gmail.com>
2014-07-14 20:47:04 +0200

    update to new approach and faster functions

--- 5.Meridhov/meridhov.py
+++ 5.Meridhov/meridhov.py
@@ -16,12 +16,14 @@ def cflon(f, altmin, latbounds):
 
     # altmin is an array
     
-    v = vcm.VCM(f)
+    v = vcm.VCM(f, verbose=False)
     out = da.Dataset()
+
+    lon_axis = da.Axis(lonbins[:-1], 'lon')
 
     # number of profiles per lon bin
     h, xx = np.histogram(v.lon, bins=lonbins)
-    out['nprof'] = da.DimArray(h, labels=[lonbins[:-1]], dims=['lon',])
+    out['nprof'] = da.DimArray(h, [lon_axis])
     out['nprof'].longname = 'Number of measured profiles'
     
     for n in names:
@@ -31,18 +33,20 @@ def cflon(f, altmin, latbounds):
         
         # clip latitudes
         latidx = (v.lat >= latbounds[0]) & (v.lat < latbounds[1])
-        cv = cv[latidx,:]
+        cv = np.take(cv, latidx, axis=0)
+        lon = np.take(v.lon, latidx, axis=0)
         
         outdict = dict()
         
         for a in altmin:
             
-            idx = (v.altitude >= a) 
-            cloudy = np.sum(cv[:,idx], axis=1)
+            idx = np.where(v.altitude >= a)[0]
+            cloudy = np.take(cv, idx, axis=1)
+            cloudy = np.sum(cloudy, axis=1)
             np.clip(cloudy, 0, 1, out=cloudy)
             
-            h, xx = np.histogram(v.lon[latidx], bins=lonbins, weights=cloudy)
-            outdict[a] = da.DimArray(h, labels=[lonbins[:-1]], dims=['lon',])
+            h, xx = np.histogram(v.lon, bins=lonbins, weights=cloudy)
+            outdict[a] = da.DimArray(h, [lon_axis,])
         
         outname = n + '_cprof'
         out[outname] = da.stack(outdict, axis='altmin')
@@ -55,18 +59,7 @@ def cflon_files(files, altmin, latbounds, outname, where):
     
     import os
     
-    dataset = None
-    for f in files:
-        out = cflon(f, altmin, latbounds)
-        if out is None:
-            return
-        if dataset is None:
-            dataset = out
-            fields = dataset.keys()
-        else:
-            for field in fields:
-                dataset[field] += out[field]
-    
+    dataset = cflon(files, altmin, latbounds)
     if not os.path.isdir(where):
         print('Creating output dir ' + where)
         os.mkdir(where)
